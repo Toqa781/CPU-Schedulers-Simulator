457b192a06034d6a41466bd98d75b6c2e3917831
Kernashaat203 <147157199+Kernashaat203@users.noreply.github.com>
2024-12-11 00:22:08 +0200

    Update Process.java

--- src/src/Process.java
+++ src/src/Process.java
@@ -1,38 +1,40 @@
 import java.awt.*;
 
 public class Process {
-    String name;               
-    Color color;               
-    int arrivalTime;           
-    int burstTime;             
-    int priority;              
-    int completionTime;        
-    int waitingTime;          
-    int turnaroundTime;        
-    boolean isCompleted;      
-    int remainingBurstTime;    
-    int waitingTimeCounter;    
+    String name;
+    Color color;
+    int arrivalTime;
+    int burstTime;
+    int priority;
+    int completionTime;
+    int waitingTime;
+    int turnaroundTime;
+    boolean isCompleted;
+    int remainingBurstTime;
+    int waitingTimeCounter;
+    int startTime;
+    int endTime;
     // SJF
     public Process(String name, Color color, int arrivalTime, int burstTime) {
         this.name = name;
         this.color = color;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
-        this.remainingBurstTime = burstTime; 
+        this.remainingBurstTime = burstTime;
         this.isCompleted = false;
-        this.waitingTimeCounter = 0;         
+        this.waitingTimeCounter = 0;
     }
 
-    // priority 
+    // priority
     public Process(String name, Color color, int arrivalTime, int burstTime, int priority) {
-        this(name, color, arrivalTime, burstTime); 
+        this(name, color, arrivalTime, burstTime);
         this.priority = priority;
     }
-    
+
     public void calculateTurnaroundTime() {
         this.turnaroundTime = this.completionTime - this.arrivalTime;
     }
-    
+
     public void calculateWaitingTime() {
         this.waitingTime = this.turnaroundTime - this.burstTime;
     }
@@ -40,9 +42,9 @@ public class Process {
     // Aging of sjf for starvation problem
     public void applyAging(int agingThreshold) {
         if (waitingTimeCounter >= agingThreshold) {
-            this.remainingBurstTime--; 
-            this.remainingBurstTime = Math.max(this.remainingBurstTime, 1); 
-            this.waitingTimeCounter = 0; 
+            this.remainingBurstTime--;
+            this.remainingBurstTime = Math.max(this.remainingBurstTime, 1);
+            this.waitingTimeCounter = 0;
         }
     }
 
